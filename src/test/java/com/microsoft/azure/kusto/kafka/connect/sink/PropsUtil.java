1e50af86a4b1bbc4d54bedcc4e5149b2014eced2
hasher <anmol.bansal@hashedin.com>
2020-06-04 17:39:16 +0530

    clean PropsUtil

--- src/test/java/com/microsoft/azure/kusto/kafka/connect/sink/PropsUtil.java
+++ src/test/java/com/microsoft/azure/kusto/kafka/connect/sink/PropsUtil.java
@@ -10,10 +10,10 @@ public class PropsUtil {
     props.put("topics","mytopic");
     props.put("tasks.max","1");
     props.put("kusto.tables.topics_mapping","[{'topic': 'mytopic','db': 'TestConnectorDB', 'table': 'SanchayTable','format': 'json', 'mapping':'SanchayMapping'}]");
-    props.put("kusto.url","https://ingest-azureconnector.centralus.kusto.windows.net/");
-    props.put("kusto.auth.authority","7f66d0ea-6137-4e37-a835-4530eba9b3ee");
-    props.put("kusto.auth.appid","e299f8c0-4965-4b1f-9b55-a56dd4b8f6c4");
-    props.put("kusto.auth.appkey","1t-fP8o~a0b8X7-o0.wCr6.0aPBR7~k9L");
+    props.put("kusto.url","");
+    props.put("kusto.auth.authority","");
+    props.put("kusto.auth.appid","");
+    props.put("kusto.auth.appkey","");
     props.put("kusto.sink.tempdir","/var/tmp/");
     props.put("value.converter","org.apache.kafka.connect.storage.StringConverter");
     props.put("key.converter","org.apache.kafka.connect.storage.StringConverter");
